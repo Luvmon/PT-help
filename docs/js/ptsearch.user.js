a722acf36ba253d058d2c88ef29a63f30ede3218
Luvmon <48517425+Luvmon@users.noreply.github.com>
2020-05-02 11:59:58 +0800

    Update ptsearch.user.js

--- docs/js/ptsearch.user.js
+++ docs/js/ptsearch.user.js
@@ -344,7 +344,8 @@ $(document).ready(function () {
                         "link": "https://hdcity.city/" + _tag_name.attr("href"),
                         "pubdate": Date.parse(_date),
                         "size": FileSizetoLength(_size),
-                        "seeders": _tag_seeders.text().replace(',', '') || 0,
+                        // "seeders": _tag_seeders.text().replace(',', '') || 0,
+                        "seeders": _tag_seeders.text() || 0,
                         "leechers": _tag_leechers.text().replace(',', '') || 0,
                         "completed": _tag_completed.text().replace(',', '') || 0
                     });
